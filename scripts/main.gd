b6c47859c64f2008b9d5d1a306291b81750f6c4c
WalkerV2 <37280150+WalkerV2@users.noreply.github.com>
2026-05-05 22:28:19 -0600

    Added folders
    Added desired folder structure for the Music directory

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,12 +1,29 @@
 extends Control
 
+var music_dir = OS.get_system_dir(OS.SYSTEM_DIR_MUSIC)
+# must find files in music_dir
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("U smell")
+	dir_contents(music_dir)
+	
 	pass # Replace with function body.
 
+# for directories in music_dir. get they songs, cuz them directories is artists.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func dir_contents(path):
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print(DirAccess.get_files_at(music_dir.path_join("ADO Instrumentals")))
+				print("Found directory: " + file_name)
+			else:
+				print("Found file: " + file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
