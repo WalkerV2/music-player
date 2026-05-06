724ffcc34913906249b49c8794e9b95fbc54f1bf
WalkerV2 <37280150+WalkerV2@users.noreply.github.com>
2026-05-05 23:53:02 -0600

    phentos ownt leavemealone
    leave brittney alone

--- scripts/main.gd
+++ scripts/main.gd
@@ -10,7 +10,20 @@ func _ready() -> void:
 	
 	pass # Replace with function body.
 
+# this should be safe to run at any time, so we aren't regenerating the scenes every boot.
 # for directories in music_dir. get they songs, cuz them directories is artists.
+# for Artists in music_dir:
+# 	Generate Artists.tscn.instantiate()
+# 	for [Albums,Covers,Singles] in Artists
+#		Generate albums.tscn.instantiate()
+#		Generate covers.tscn.instantiate()
+#		Generate singles.tscn.instantiate()
+#		for songs in albums
+#			Generate songs.tscn.isntantiate()
+#		for songs in covers
+#			generate songs.tscn.instantiate()
+#		for songs in singles
+#			generate songs.tscn.instantiate()
 
 
 func dir_contents(path):
@@ -20,7 +33,7 @@ func dir_contents(path):
 		var file_name = dir.get_next()
 		while file_name != "":
 			if dir.current_is_dir():
-				print(DirAccess.get_files_at(music_dir.path_join("ADO Instrumentals")))
+				print(DirAccess.get_files_at(music_dir.path_join(file_name)))
 				print("Found directory: " + file_name)
 			else:
 				print("Found file: " + file_name)
