ea7cddeaf2b2327904ff30e7ce29e738d25264c5
WalkerV2 <37280150+WalkerV2@users.noreply.github.com>
2026-05-06 00:02:28 -0600

    Added 2 nodes
    nodes added 2

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,15 @@
 extends Control
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("U smell")
-	pass # Replace with function body.
+
+var music_dir = OS.get_system_dir(OS.SYSTEM_DIR_MUSIC)
+
+# must find files in music_dir
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
