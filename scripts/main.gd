b32d3d4bfa912e6c12a5cfdfe99a2ba025eff784
WalkerV2 <37280150+WalkerV2@users.noreply.github.com>
2026-05-20 03:40:17 -0600

    Added clickable song  list.
    also added placeholders for the general ui layout.

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,14 @@
 extends Control
 
+@onready var songs_list: RichTextLabel = $"THEORY CRAFTING/HBoxContainer/SongsDisplay/VBoxContainer/SongsList"
+
 @onready var file_dialog: FileDialog = $FileDialog
+
 var audio = preload("res://scenes/audio_player.tscn").instantiate()
 var CONFIG_PATH = "user://music-player.cfg"
 var music_dir: String = OS.get_system_dir(OS.SYSTEM_DIR_MUSIC)
 var config = ConfigFile.new()
-# for albums in config.get_sections() 
+
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
@@ -23,14 +26,14 @@ func build_library(path: String) -> void:
 		var artist_path = music_dir.path_join(artist_folder)
 		artist.title = artist_folder
 		artist.folder = music_dir.path_join(artist_folder)
-		print(artist.folder)
+		#print(artist.folder)
 		for album_folder in DirAccess.get_directories_at(artist_path):
 			var album = AlbumData.new()
 			var album_path = artist_path.path_join(album_folder)
 			album.title = album_folder
 			album.folder = artist_folder.path_join(album_folder)
 			artist.albums.append(album)
-			print(album_folder)
+			#print(album_folder)
 			for song_file in DirAccess.get_files_at(album_path):
 				var song = SongData.new()
 				song.title = song_file.get_basename()
@@ -45,4 +48,18 @@ func build_library(path: String) -> void:
 
 
 func _on_file_dialog_button_pressed() -> void:
-	build_library(music_dir)
+	var bbcode = ""
+	#build_library(music_dir)
+	var artist = load("user://artists/ADO.tres")
+	for album in artist.albums:
+		for song in album.songs:
+			#[url={"key": "value"}]Text[/url] everything after the = is the key.
+			# ALL clickable metas connect to the same func.
+			# like linking to the artists scene, the album scene. or whatever else you may want.
+			bbcode += "[url=play" + song.file + "]" + song.title + "[/url]" + "\n"
+		songs_list.text = bbcode
+
+
+func _on_songs_list_meta_clicked(meta: Variant) -> void:
+	print(meta.trim_prefix("play"))
+	pass # Replace with function body.
